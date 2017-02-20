b22240197fdd4e56545d5467b0b3f64907c13ecb
Kuba Skwara <skwaraj@student,agh.edu.pl>
2017-02-20 22:01:07 +0100

    Added basic activites: UserLoginActivity, FlatLoginActivity and MainActivity

---
+++ app/src/main/java/com/dreamsfactory/dutiesmanager/activities/FlatLoginActivity.java
@@ -0,0 +1,15 @@
+package com.dreamsfactory.dutiesmanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dreamsfactory.dutiesmanager.R;
+
+public class FlatLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flat_login);
+    }
+}
