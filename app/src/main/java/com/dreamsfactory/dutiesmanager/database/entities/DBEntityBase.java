a6563db650f6f72835b5528bca7793090f85b08c
Kuba Skwara <skwaraj@student,agh.edu.pl>
2017-02-22 12:44:49 +0100

    Created abstract class DBEntityBase for entities

---
+++ app/src/main/java/com/dreamsfactory/dutiesmanager/database/entities/DBEntityBase.java
@@ -0,0 +1,37 @@
+package com.dreamsfactory.dutiesmanager.database.entities;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Kuba on 2017-02-22.
+ */
+
+public abstract class DBEntityBase implements BaseColumns {
+
+    //constants
+    protected static String TEXT_TYPE = " TEXT";
+    protected static String INTEGER_TYPE = " INTEGER";
+    protected static String FLOAT_TYPE = " REAL";
+    protected static String PIMARY_KEY = " PRIMARY KEY";
+    protected static String COMMA = ", ";
+
+
+    //variables
+    protected long id;
+
+    //methods
+    public abstract ContentValues getContentValues();
+    public abstract boolean readFromCursor(Cursor cursor);
+    public abstract String getTableName();
+
+    public long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+
+
+}
