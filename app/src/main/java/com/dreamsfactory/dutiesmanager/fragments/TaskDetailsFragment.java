84b8a60ef26930064823fc244bdfcdce0e792027
Kuba Skwara <skwaraj@student,agh.edu.pl>
2017-03-01 13:03:28 +0100

    Added fragment for task details

---
+++ app/src/main/java/com/dreamsfactory/dutiesmanager/fragments/TaskDetailsFragment.java
@@ -0,0 +1,30 @@
+package com.dreamsfactory.dutiesmanager.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dreamsfactory.dutiesmanager.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TaskDetailsFragment extends Fragment {
+
+
+    public TaskDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_details, container, false);
+    }
+
+}
