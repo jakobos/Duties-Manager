b143dbf8e0445b5d600905374c9f7cadd4ef30e1
Kuba Skwara <skwaraj@student,agh.edu.pl>
2017-03-03 21:45:45 +0100

    Added posibility to set task details

--- app/src/main/java/com/dreamsfactory/dutiesmanager/fragments/TaskDetailsFragment.java
+++ app/src/main/java/com/dreamsfactory/dutiesmanager/fragments/TaskDetailsFragment.java
@@ -2,17 +2,30 @@ package com.dreamsfactory.dutiesmanager.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dreamsfactory.dutiesmanager.R;
+import com.dreamsfactory.dutiesmanager.database.entities.Task;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TaskDetailsFragment extends Fragment {
+
+    private Task task;
+
+    private TextView title;
+    private TextView time;
+    private TextView ownerText;
+    private TextView ownerName;
+    private TextView description;
+
+    public static final String KEY_TASK = "task";
 
 
     public TaskDetailsFragment() {
@@ -23,8 +36,41 @@ public class TaskDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_details, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_task_details, container, false);
+
+        title = (TextView) view.findViewById(R.id.taskTitle);
+        time = (TextView) view.findViewById(R.id.taskTime);
+        ownerText = (TextView) view.findViewById(R.id.taskOwnerText);
+        ownerName = (TextView) view.findViewById(R.id.taskOwnerName);
+        description = (TextView) view.findViewById(R.id.taskDescription);
+
+
+        return view;
     }
+
+    public void setTask(Task task){
+        this.task = task;
+        setData();
+
+    }
+    private void setData(){
+        title.setText(task.getTitle());
+        time.setText(task.getCountdown());
+        if(task.getOwnerId() > 0){
+            ownerText.setVisibility(View.VISIBLE);
+            ownerName.setVisibility(View.VISIBLE);
+            //set owner name
+        }
+        description.setText(task.getDescription());
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+       // outState.putParcelable(KEY_TASK, task);
+    }
+
 
 }
