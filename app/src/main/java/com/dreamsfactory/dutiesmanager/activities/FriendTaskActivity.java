e5eafbde7b727f0e0a7bfd76197a25ba2cc90fd8
Kuba Skwara <skwaraj@student,agh.edu.pl>
2017-03-04 22:20:43 +0100

    Added getting Task from intent and saving instance state

--- app/src/main/java/com/dreamsfactory/dutiesmanager/activities/FriendTaskActivity.java
+++ app/src/main/java/com/dreamsfactory/dutiesmanager/activities/FriendTaskActivity.java
@@ -1,5 +1,6 @@
 package com.dreamsfactory.dutiesmanager.activities;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,15 +11,29 @@ import com.dreamsfactory.dutiesmanager.fragments.TaskDetailsFragment;
 public class FriendTaskActivity extends AppCompatActivity {
 
     private Task task;
+    private static final String KEY_TASK = "task";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_task);
 
-        task = new Task();
+        task = getIntent().getExtras().getParcelable(TaskDetailsFragment.KEY_TASK);
+        //task = new Task();
 
-        TaskDetailsFragment fragment = (TaskDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentTaskDetails);
+        TaskDetailsFragment fragment = (TaskDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentFriendTaskDetails);
         fragment.setTask(task);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putParcelable(KEY_TASK, task);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        task = (Task) savedInstanceState.getParcelable(KEY_TASK);
     }
 }
