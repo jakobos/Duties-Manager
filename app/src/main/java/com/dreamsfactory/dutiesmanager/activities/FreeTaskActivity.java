a3d7e673eee2c1b504312e8cad3e52988c04dc60
Kuba Skwara <skwaraj@student,agh.edu.pl>
2017-03-16 17:11:29 +0100

    Added checkbox'es on click methods

--- app/src/main/java/com/dreamsfactory/dutiesmanager/activities/FreeTaskActivity.java
+++ app/src/main/java/com/dreamsfactory/dutiesmanager/activities/FreeTaskActivity.java
@@ -3,26 +3,50 @@ package com.dreamsfactory.dutiesmanager.activities;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 import com.dreamsfactory.dutiesmanager.R;
 import com.dreamsfactory.dutiesmanager.database.entities.Task;
 import com.dreamsfactory.dutiesmanager.fragments.TaskDetailsFragment;
+import com.dreamsfactory.dutiesmanager.settings.Settings;
+import com.dreamsfactory.dutiesmanager.webServices.WebServiceManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FreeTaskActivity extends AppCompatActivity {
 
     private Task task;
     private static final String KEY_TASK = "task";
 
+    private CheckBox checkBoxDoIt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_task);
+
+        checkBoxDoIt = (CheckBox) findViewById(R.id.checkDoIt);
 
         task = getIntent().getExtras().getParcelable(TaskDetailsFragment.KEY_TASK);
         //task = new Task();
 
         TaskDetailsFragment fragment = (TaskDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentFreeTaskDetails);
         fragment.setTask(task);
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(checkBoxDoIt.isChecked()){
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("task_id", String.valueOf(task.getTaskId()));
+            params.put("owner_id", Settings.getInstance(getBaseContext()).get(Settings.USER_ID));
+            WebServiceManager.getInstance(getBaseContext()).updateTask(params);
+        }
     }
 
     @Override
--- app/src/main/java/com/dreamsfactory/dutiesmanager/activities/FriendTaskActivity.java
+++ app/src/main/java/com/dreamsfactory/dutiesmanager/activities/FriendTaskActivity.java
@@ -3,26 +3,47 @@ package com.dreamsfactory.dutiesmanager.activities;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 import com.dreamsfactory.dutiesmanager.R;
 import com.dreamsfactory.dutiesmanager.database.entities.Task;
 import com.dreamsfactory.dutiesmanager.fragments.TaskDetailsFragment;
+import com.dreamsfactory.dutiesmanager.settings.Settings;
+import com.dreamsfactory.dutiesmanager.webServices.WebServiceManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FriendTaskActivity extends AppCompatActivity {
 
     private Task task;
     private static final String KEY_TASK = "task";
 
+    private CheckBox checkBoxIsDone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_task);
+
+        checkBoxIsDone = (CheckBox) findViewById(R.id.checkIsDone);
 
         task = getIntent().getExtras().getParcelable(TaskDetailsFragment.KEY_TASK);
         //task = new Task();
 
         TaskDetailsFragment fragment = (TaskDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentFriendTaskDetails);
         fragment.setTask(task);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(checkBoxIsDone.isChecked()){
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("task_id", String.valueOf(task.getTaskId()));
+            params.put("is_done", String.valueOf(1));
+            WebServiceManager.getInstance(getBaseContext()).updateTask(params);
+        }
     }
 
     @Override
